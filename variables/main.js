eefa66bcf2a99b7b11dc21e32216717f04f13129
Anastasia Bulankina <anastessha@gmail.com>
2019-11-21 01:39:18 +0900

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,26 @@
+'use strict'
+
+//=====Задача 1=======
+let algebra = 4,
+geography = 5,
+physics = 3;
+
+let average = (algebra + geography + physics) / 3;
+console.log('Средняя оценка по трем предметам: ' + average);
+
+console.log();
+//=====Задача 2=======
+let myName = 'Анастасия';
+let message = `Привет, мир! Меня зовут ${myName}!`;
+
+console.log(message);
+
+console.log();
+//=====Задача 3=======
+let x = 2,
+y = 22,
+z = 0;
+
+let result = x * y + 5 * z + x - 1;
+
+console.log(result);

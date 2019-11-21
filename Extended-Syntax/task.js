4a172dd516768d2eac4653d2bcb2fd3293a8e4b7
Anastasia Bulankina <anastessha@gmail.com>
2019-11-21 18:32:15 +0900

    first commit Extended-Syntax

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -1,4 +1,4 @@
-
+'use strict'
 
 function calculateQuadraticEquation(){
     let a = +window.a.value;
@@ -10,8 +10,20 @@ function calculateQuadraticEquation(){
     span.textContent = "х = "+result;
 }
 
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
+function getResult(a,b,c) {
+    
+  // код для задачи №1 писать здесь
+    
+  let d = b * b - 4 * a * c;
+  let x = [];
+  if (d < 0) {
+    return x;
+  } else if (d > 0) {
+    return [(-b + Math.sqrt(d)) / 2 * a, (-b - Math.sqrt(d)) / 2 * a];
+  } else {
+    return [-b / 2 * a];
+  }
+    
     //return x;
 }
 
@@ -21,10 +33,27 @@ function calculateAverageRating(){
     window.averageMark.textContent = averageMark;
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    //return averageMark;
-}
+function getAverageMark(marks) {
+  
+  // код для задачи №2 писать здесь
+
+  let sum = 0;
+  let arrayTransf = marks.splice(5);
+
+  if (marks.length <= 5) {
+    for (let i = 0; i < marks.length; i++) {
+      sum += marks[i];
+    }
+    return sum / marks.length;
+
+  } else if (marks.length > 5) {
+    for (let i = 0; i < arrayTransf.length; i++) {
+      sum += arrayTransf[i]; 
+    }
+   return sum / arrayTransf.length;
+  }
+}  
+
 
 function calculateDrinkTask(){
     let name = window.personName.value;
@@ -33,8 +62,20 @@ function calculateDrinkTask(){
     window.drink.textContent = drink;
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    //console.log(result)
-    //return result;
+function askDrink(name,dateOfBirthday) {
+    
+  // код для задачи №3 писать здесь
+    
+    let year = new Date().getFullYear();
+    let yearBirth = dateOfBirthday.getFullYear();
+    let age = year - yearBirth;
+    let result;
+        
+    if (age >= 18) {
+        result = `Не желаете ли олд-фэшн, ${name}?`;
+    } else {
+        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
+    
+    return result;
 }

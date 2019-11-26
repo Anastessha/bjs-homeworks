cdacadbad27c402b31ba2413f424fc05171bf719
Anastasia Bulankina <anastessha@gmail.com>
2019-11-27 04:49:44 +0900

    first commit wrappers-for-primitives

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+'use strict'
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -11,9 +13,18 @@ function calculateMortgage() {
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+    let mortgageEndDay = new Date(date),
+    today = new Date(),
+    months = mortgageEndDay.getMonth() - today.getMonth() + ((mortgageEndDay.getFullYear() - today.getFullYear()) * 12);
+            
+    let p = percent / (12 * 100),
+    payOfMonth = (amount - contribution) * (p + p / (((1 + p) ** months) - 1)),
+    totalAmount = payOfMonth * months;
+    
+    console.log(totalAmount);
+    return totalAmount.toFixed(2);
 }
+
 
 function sayHello() {
     let name = window.personName.value;
@@ -23,6 +34,15 @@ function sayHello() {
 }
 
 function getGreeting(name) {
+    
     // код для задачи №2 писать здесь
-    //return greeting;
+    
+    let greeting;
+    
+    if (name == null || name == undefined || name == '') {
+        greeting = `Привет, мир! Меня зовут Аноним`;
+    } else {
+        greeting = `Привет, мир! Меня зовут ${name}`;
+    }
+    return greeting;
 }
